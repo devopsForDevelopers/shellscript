7cc8cae226589bcd56097ba9330ab526a527a208
Janak <mandani.janak15@gmail.com>
2024-03-23 22:02:19 +0530

    added shell script for date and uptime

---
+++ dateandtime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This is my first shell script"
+
+#date command in Linux is used to display current date and time
+date
+
+#uptime command in Linux is a powerful tool that displays the current time, the system's uptime(how long the system has been running), the number of users, and the load average.
+uptime
